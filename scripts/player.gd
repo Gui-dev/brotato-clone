087d1a01ed2f876f5c2a89b168af12166fcb5ebc
Adriano Silva <adrianon10@gmail.com>
2024-04-07 05:09:28 -0300

    chore: add first commit

---
+++ scripts/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+class_name Player
+
+
+@export_category("variables")
+@export var _move_speed: float = 256.0
+
+
+func _physics_process(delta: float) -> void:
+  _move()
+  
+  
+func _move() -> void:
+  var _direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+  velocity = _direction * _move_speed
+  move_and_slide()
